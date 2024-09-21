8268820bc723e4a6997f05e3001e10594bd9bb80
Codngwthubaid <codngwthubaid@gmail.com>
2024-09-22 00:01:04 +0530

    Add breadcrumb component in the about page in the top of the section

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -10,32 +10,35 @@ import {
 
 
 const page = () => {
-  return (<>
-    <main>
-      <section>
-        <article>
-          <div>
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-
-          </div>
-        </article>
-      </section>
-    </main>
-  </>
+  return (
+    <>
+      <main>
+        <section>
+          <article className='pt-[75px]'>
+            <div className='bg-[#a99595] gap-y-5 flex flex-col justify-center items-center border-2 border-green-800 h-[50vh] text-white'>
+              <div className='text-5xl font-bold text-[#ffedd5]'>About Us</div>
+              <div>
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+              </div>
+            </div>
+          </article>
+        </section>
+      </main>
+    </>
   )
 }
 
