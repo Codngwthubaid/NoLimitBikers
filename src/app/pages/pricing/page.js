e0c1c75fd96e16872b75921aade7554b94a3371c
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-19 15:03:27 +0530

    Add the popup functionality by which the user buy the packages by filling their details

--- src/app/pages/pricing/page.js
+++ src/app/pages/pricing/page.js
@@ -21,16 +21,21 @@ import {
 } from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
+import { TriangleAlert } from 'lucide-react';
 
 const page = () => {
-
-
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const [formError, setFormError] = useState("");
 
-    const onSubmit = (data) => console.log(data)
-
+    const onSubmit = (data) => {
+        if (Object.keys(errors).length === 0) {
+            console.log(data);
+        } else {
+            console.log("Please fill all required fields");
+        }
+    }
 
     return (
         <div>
@@ -66,34 +71,25 @@ const page = () => {
                                         <DialogTrigger asChild>
                                             <Button className="bg-orange-500 hover:text-white hover:bg-orange-600 text-white" variant="outline">Beginnen met basis</Button>
                                         </DialogTrigger>
-                                        <DialogContent className="bg-[#ffedd5] sm:max-w-[425px]">
+                                        <DialogContent className="bg-[#ffedd5] sm:max-w-[425px]" onSubmit={handleSubmit(onSubmit)}>
                                             <DialogHeader>
                                                 <DialogTitle className="font-bold text-4xl text-orange-400">Book Your Lesson</DialogTitle>
                                                 <DialogDescription className="text-black text-base">
                                                     Book your lesson today! Choose your preferred time for driving lessons and start your journey towards becoming a confident driver!
                                                 </DialogDescription>
                                             </DialogHeader>
-                                            <div className="grid gap-4 py-4" onSubmit={handleSubmit(onSubmit)}>
+                                            <form className="grid gap-4 py-4">
                                                 <div className="flex flex-col space-y-1.5 items-start">
-                                                    <Label htmlFor="name" className="text-base text-orange-400">
-                                                        Name
-                                                    </Label>
-                                                    <Input id="name" className="col-span-3" {...register("name", { required: true })} />
-                                                    {errors.name && <span>This field is required</span>}
+                                                    <Input placeholder="Name" id="name" className="col-span-3" {...register("name", { required: true })} />
+                                                    {errors.name && <span className="text-red-500 font-semibold flex"><TriangleAlert /> <span>Please enter your name</span></span>}
                                                 </div>
                                                 <div className="flex flex-col space-y-1.5 items-start">
-                                                    <Label htmlFor="email" className="text-base text-orange-400">
-                                                        Email
-                                                    </Label>
-                                                    <Input id="email" className="col-span-3" {...register("email", { required: true })} />
-                                                    {errors.email && <span>This field is required</span>}
+                                                    <Input placeholder="Email" id="email" className="col-span-3" {...register("email", { required: true })} />
+                                                    {errors.email && <span className="text-red-500 font-semibold flex"><TriangleAlert /> <span>Please enter your name</span></span>}
                                                 </div>
                                                 <div className="flex flex-col space-y-1.5 items-start">
-                                                    <Label htmlFor="number" className="text-base text-orange-400">
-                                                        Number
-                                                    </Label>
-                                                    <Input id="number" className="col-span-3" {...register("number", { required: true })} />
-                                                    {errors.number && <span>This field is required</span>}
+                                                    <Input placeholder="Number" id="number" className="col-span-3" {...register("number", { required: true })} />
+                                                    {errors.number && <span className="text-red-500 font-semibold flex"><TriangleAlert /> <span>Please enter your number</span></span>}
                                                 </div>
                                                 <div className="flex flex-col space-y-1.5 items-start">
                                                     <Label htmlFor="framework" className="text-base text-orange-400">Select Course</Label>
@@ -102,19 +98,29 @@ const page = () => {
                                                             <SelectValue />
                                                         </SelectTrigger>
                                                         <SelectContent className="bg-[#ffedd5]" position="popper">
-                                                            <SelectItem className="cursor-pointer" value="next">Next.js</SelectItem>
-                                                            <SelectItem className="cursor-pointer" value="sveltekit">SvelteKit</SelectItem>
-                                                            <SelectItem className="cursor-pointer" value="astro">Astro</SelectItem>
-                                                            <SelectItem className="cursor-pointer" value="nuxt">Nuxt.js</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVB Pakket Silver">AVB Pakket Silver</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVB Pakket Gold Mega Deal">AVB Pakket Gold Mega Deal</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVB Pakket Platinum">AVB Pakket Platinum</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVD Pakket Silver">AVD Pakket Silver</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVD Pakket Gold Mega Deal">AVD Pakket Gold Mega Deal</SelectItem>
+                                                            <SelectItem className="cursor-pointer" value="AVD Pakket Platinum">AVD Pakket Platinum</SelectItem>
                                                         </SelectContent>
                                                     </Select>
                                                 </div>
-                                            </div>
+                                            </form>
                                             <DialogFooter className="flex justify-between">
                                                 <DialogTrigger>
                                                     <Button variant="outline">Cancel</Button>
                                                 </DialogTrigger>
-                                                <Button type="submit" className="bg-orange-400 hover:bg-[#a99595]">Submit</Button>
+                                                <Button type="button" className="bg-orange-400 hover:bg-[#a99595]" onClick={() => {
+                                                    handleSubmit((data) => {
+                                                        if (Object.keys(errors).length === 0) {
+                                                            console.log(data);
+                                                        } else {
+                                                            setFormError("Please fill all required fields");
+                                                        }
+                                                    })();
+                                                }}>Submit</Button>
                                             </DialogFooter>
                                         </DialogContent>
                                     </Dialog>
@@ -173,7 +179,7 @@ const page = () => {
                         <div className="flex flex-col lg:flex-row gap-8">
                             <Card className="flex flex-col items-center  bg-white">
                                 <CardHeader>
-                                    <CardTitle className="text-gray-800 text-lg">AVB Pakket Silver</CardTitle>
+                                    <CardTitle className="text-gray-800 text-lg">AVD Pakket Silver</CardTitle>
                                 </CardHeader>
                                 <CardContent className="text-center">
                                     <p className="text-4xl font-bold text-gray-800 mt-4">€799,00</p>
@@ -196,7 +202,7 @@ const page = () => {
                                     Recommended
                                 </div>
                                 <CardHeader>
-                                    <CardTitle className="text-lg mt-4">AVB Pakket Gold Mega Deal</CardTitle>
+                                    <CardTitle className="text-lg mt-4">AVD Pakket Gold Mega Deal</CardTitle>
                                 </CardHeader>
                                 <CardContent className="text-center">
                                     <p className="text-4xl font-bold mt-4">€1199,00</p>
@@ -216,7 +222,7 @@ const page = () => {
 
                             <Card className="flex flex-col items-center  bg-orange-100">
                                 <CardHeader>
-                                    <CardTitle className="text-gray-800 text-lg">AVB Pakket Platinum</CardTitle>
+                                    <CardTitle className="text-gray-800 text-lg">AVD Pakket Platinum</CardTitle>
                                 </CardHeader>
                                 <CardContent className="text-center">
                                     <p className="text-4xl font-bold text-gray-800 mt-4">€1299,00</p>
