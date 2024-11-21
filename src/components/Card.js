85e540941a462b958626194498aa8b42f690fb26
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-21 11:28:53 +0530

    The Second Section is content is converted into dutch from englsih

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Card = (props) => {
     return (
         <>
-            <div className='flex flex-col justify-center items-center text-center gap-y-3 border border-orange-300 py-5 rounded-2xl hover:text-orange-400'>
+            <div className='flex flex-col justify-center items-center text-center gap-y-3 border border-orange-300 py-5 rounded-2xl hover:text-orange-400 h-60'>
                 <div>
                     <Image
                         src={props.img}
@@ -16,7 +16,7 @@ const Card = (props) => {
                 <div className='font-semibold text-xl'>{props.data}</div>
                 <div>{props.details}</div>
                 <div className='flex justify-center items-center gap-x-2 cursor-pointer'>
-                    <Link href="/pages/about" className='font-semibold underline text-sm'>READ MORE</Link>
+                    <Link href="/pages/about" className='font-semibold underline text-sm'>LEES MEER</Link>
                 </div>
             </div>
         </>
--- src/components/SectionSecond.js
+++ src/components/SectionSecond.js
@@ -12,24 +12,24 @@ const SectionSecond = () => {
                 <section className="container mx-auto">
                     <article>
                         <div className="flex justify-center items-center flex-col gap-y-3">
-                            <div className="text-2xl font-semibold text-orange-400">Our Services</div>
-                            <div className="text-4xl font-bold">We Provide Service For You</div>
+                            <div className="text-2xl font-semibold text-orange-400">Onze diensten</div>
+                            <div className="text-4xl font-bold">Wij bieden service voor u</div>
                         </div>
                         <div className="flex flex-col lg:flex-row gap-5 my-10">
                             <div className="flex gap-x-5 justify-center items-center gap-y-5 flex-col sm:flex-row">
                                 <div className="w-10/12 sm:w-[45vw] lg:w-[22vw]">
-                                    <Card img={ELD} data="Easy Learn Driving" details="Our Course module is very easy to understand." />
+                                    <Card img={ELD} data="Gemakkelijk leren autorijden" details="Onze Cursusmodule is heel gemakkelijk te begrijpen" />
                                 </div>
                                 <div className="w-10/12 sm:w-[45vw] lg:w-[22vw]">
-                                    <Card img={IT} data="Instructor Training" details="Brainstrom words related to your skills and confidence." />
+                                    <Card img={IT} data="Instructeursopleiding" details="Brainstorm over woorden die verband houden met uw vaardigheden en zelfvertrouwen." />
                                 </div>
                             </div>
                             <div className="flex gap-x-5 justify-center items-center flex-col sm:flex-row gap-y-5">
                                 <div className="w-10/12 sm:w-[45vw] lg:w-[22vw]">
-                                    <Card img={CYSAT} data="Easy Learn Driving" details="Our Course module is very easy to understand." />
+                                    <Card img={CYSAT} data="Gemakkelijk leren autorijden" details="Onze Cursusmodule is heel gemakkelijk te begrijpen." />
                                 </div>
                                 <div className="w-10/12 sm:w-[45vw] lg:w-[22vw]">
-                                    <Card img={RTP} data="Easy Learn Driving" details="Our Course module is very easy to understand." />
+                                    <Card img={RTP} data="Gemakkelijk leren autorijden" details="Onze Cursusmodule is heel gemakkelijk te begrijpen." />
                                 </div>
                             </div>
                         </div>
