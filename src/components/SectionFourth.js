b151783418dea52793bd56e1073346ecc44a751b
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-21 11:45:20 +0530

    Section Fourth is converted from english to dutch

--- src/components/SectionFourth.js
+++ src/components/SectionFourth.js
@@ -12,24 +12,24 @@ const SectionFourth = () => {
                     <article className="flex lg:justify-around justify-center items-center bg-[#a99595] py-10 flex-col md:flex-row gap-y-10">
                         <div className="lg:w-[50vw] w-11/12 flex flex-col gap-y-10">
                             <div className="flex flex-col justify-center items-start gap-y-5">
-                                <div className="text-2xl text-orange-400 font-semibold">Why Choosing Us</div>
-                                <div className="text-5xl font-bold text-[#ffedd5]">Get Behind The Wheel And Take The Lead</div>
+                                <div className="text-2xl text-orange-400 font-semibold">Waarom voor ons kiezen</div>
+                                <div className="text-5xl font-bold text-[#ffedd5]">Kruip achter het stuur en neem de leiding</div>
                             </div>
                             <div className="flex flex-col justify-center items-start gap-y-5">
                                 <div className="flex justify-around items-center gap-x-2">
                                     <div>
-                                        <CardWCU src={rightTick} heading="Affordable Fee" details="All of our course fee are very affordable for everyone." />
+                                        <CardWCU src={rightTick} heading="Betaalbare vergoeding" details="Al onze cursuskosten zijn voor iedereen zeer betaalbaar." />
                                     </div>
                                     <div>
-                                        <CardWCU src={rightTick} heading="Affordable Fee" details="All of our course fee are very affordable for everyone." />
+                                        <CardWCU src={rightTick} heading="Offline volgen" details="Offline volgen is het proces van het verzamelen van informatie." />
                                     </div>
                                 </div>
                                 <div className="flex justify-around items-center gap-x-2">
                                     <div>
-                                        <CardWCU src={rightTick} heading="Affordable Fee" details="All of our course fee are very affordable for everyone." />
+                                        <CardWCU src={rightTick} heading="Beste trainers" details="Wij hebben de beste trainersvorm die u altijd helpt." />
                                     </div>
                                     <div>
-                                        <CardWCU src={rightTick} heading="Affordable Fee" details="All of our course fee are very affordable for everyone." />
+                                        <CardWCU src={rightTick} heading="Perfecte timing" details="Onze cursus is niet zo lang en niet zo kort dat hij perfect is." />
                                     </div>
                                 </div>
                             </div>
