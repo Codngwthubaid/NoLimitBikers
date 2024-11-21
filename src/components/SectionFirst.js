ee4cd8de0a1b326e59c8473415814b06d20909d0
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-21 11:19:08 +0530

    Convert the english text to dutch for section first

--- src/components/SectionFirst.js
+++ src/components/SectionFirst.js
@@ -39,15 +39,15 @@ const SectionFirst = () => {
                     <article className="flex justify-center items-center flex-col gap-y-5 lg:flex-row">
                         <div className="flex flex-col justify-center items-center lg:items-start gap-y-12 lg:w-[50vw] w-[95vw]">
                             <div className=" flex flex-col gap-y-4">
-                                <div className="text-xl font-semibold">Grow up your driving skills</div>
-                                <div className="text-3xl sm:text-5xl font-bold">Learn to
-                                    <span className="rounded-xl bg-orange-400 text-white px-3 mx-2">Drive With</span>
+                                <div className="text-xl font-semibold">Ontwikkel uw rijvaardigheid</div>
+                                <div className="text-3xl sm:text-5xl font-bold">Leer
+                                    <span className="rounded-xl bg-orange-400 text-white px-3 mx-2">ermee rijden</span>
                                 </div>
-                                <div className="text-3xl sm:text-5xl font-bold">Confidence</div>
+                                <div className="text-3xl sm:text-5xl font-bold">Vertrouwen</div>
                             </div>
                             <div>
                                 <Link href="/pages/courses">
-                                    <button type="button" className="text-white flex justify-center items-end gap-x-2 bg-gradient-to-br from-pink-400 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-600 font-medium rounded-lg text-sm px-5 py-4 text-center me-2 mb-2">EXPLORE COURSES
+                                    <button type="button" className="text-white flex justify-center items-end gap-x-2 bg-gradient-to-br from-pink-400 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-600 font-medium rounded-lg text-sm px-5 py-4 text-center me-2 mb-2">ONTDEK CURSUSSEN
                                         <Image
                                             src={rightArrowWhite}
                                             width={18}
