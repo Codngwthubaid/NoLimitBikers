c123d7dec88cb2492ee1285817c992e966a04b19
Codngwthubaid <codngwthubaid@gmail.com>
2024-11-22 20:03:59 +0530

    Change the entire content from english to dutch for ppricing page

--- src/app/pages/pricing/page.js
+++ src/app/pages/pricing/page.js
@@ -9,11 +9,11 @@ const page = () => {
     return (
         <div>
             <section className="body-font overflow-hidden pt-[75px]">
-                <SubSectionheadings Head="Pricing" mainPage="Home" currentPage="Pricing" />
+                <SubSectionheadings Head="Prijzen" mainPage="Thuis" currentPage="Prijzen" />
                 <div className="container px-5 py-12 mx-auto">
                     <div className="flex flex-col text-center w-full mb-10">
-                        <h1 className="text-orange-400 text-2xl font-medium title-font mb-2">Pricing Plan</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-5xl font-bold">Our Most Valuable Pricing Packages</p>
+                        <h1 className="text-orange-400 text-2xl font-medium title-font mb-2">Prijzen Plan</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-5xl font-bold">PlanOnze meest waardevolle prijspakketten</p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center bg-gradient-to-r from-orange-500 py-10 px-4">
