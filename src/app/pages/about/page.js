0181250fef2a869486dddac07ebce8a9cc40b534
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-20 10:27:38 +0530

    About page styling change

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -13,19 +13,19 @@ const Page = () => {
     <>
       <main>
         <section>
-          <article className='pt-[75px]'>
+          <article className='pt-[110px]'>
             <SubSectionheadings Head="Over ons" mainPage="Thuis" currentPage="Over ons" />
 
-            <div className="h-[2px] w-11/12 bg-orange-400 mx-auto my-14"></div>
+            <div className="h-[2px] w-11/12 bg-[#c1e1c1] mx-auto my-14"></div>
 
             <div className='mx-auto flex justify-center items-center lg:gap-x-5 lg:flex-row flex-col-reverse  gap-y-10'>
               <div className='px-6 lg:w-[50vw] flex justify-center items-start flex-col gap-y-5'>
-                <div className='text-orange-400 font-semibold text-xl'>Je krijgt meer te weten</div>
-                <div className='text-4xl lg:text-5xl font-bold'>Wij<span>&apos;</span>re Zeer ervaren en 98% studenten slagen voor het rijexamen bij de eerste poging</div>
-                <div className='text-lg'>Defensief rijden omvat het beheersen van je snelheid, vooruitkijken en voorbereid zijn op onverwachte gebeurtenissen, alert en vrij van afleiding zijn en voorbereid zijn op allerlei acties en reacties van andere bestuurders.</div>
+                <div className='text-green-400 font-semibold text-xl'>Je krijgt meer te weten</div>
+                <div className='text-4xl lg:text-5xl font-bold text-gray-700'>Wij<span>&apos;</span>re Zeer ervaren en 98% studenten slagen voor het rijexamen bij de eerste poging</div>
+                <div className='text-lg text-black'>Defensief rijden omvat het beheersen van je snelheid, vooruitkijken en voorbereid zijn op onverwachte gebeurtenissen, alert en vrij van afleiding zijn en voorbereid zijn op allerlei acties en reacties van andere bestuurders.</div>
                 <div>
                   <Link href="/pages/courses">
-                    <button type="button" className="text-white flex justify-center items-end gap-x-2 bg-gradient-to-br from-pink-400 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-600 font-medium rounded-lg text-sm px-5 py-4 text-center me-2 mb-2">ONTDEK CURSUSSEN
+                    <button type="button" className="text-white flex justify-center items-end gap-x-2 bg-green-400 p-4 rounded-xl">ONTDEK CURSUSSEN
                       <Image
                         src={rightArrowWhite}
                         width={18}
@@ -47,17 +47,14 @@ const Page = () => {
               </div>
             </div>
 
-            <div className="h-[2px] w-11/12 bg-orange-400 mx-auto my-14"></div>
+            <div className="h-[2px] w-11/12 bg-green-400 mx-auto my-14"></div>
 
             <div>
               <SectionThird />
             </div>
 
-            <div className="h-[2px] w-11/12 bg-orange-400 mx-auto my-14"></div>
-
-            <div>
-              <SectionFourth />
-            </div>
+            <div className="h-[2px] w-11/12 bg-green-400 mx-auto my-14"></div>
+            
           </article>
         </section>
       </main>
--- src/components/SubSectionheadings.js
+++ src/components/SubSectionheadings.js
@@ -12,17 +12,17 @@ const SubSectionheadings = (props) => {
     return (
         <>
             <main>
-                <div className='bg-orange-400 py-10 h-[50vh] gap-y-5 flex flex-col justify-center items-center text-white'>
-                    <div className='text-5xl font-bold text-[#ffedd5]'>{props.Head}</div>
+                <div className='bg-[#c1e1c1] py-10 h-[50vh] gap-y-5 flex flex-col justify-center items-center text-green-400'>
+                    <div className='text-5xl font-bold text-black'>{props.Head}</div>
                     <div>
                         <Breadcrumb>
                             <BreadcrumbList>
                                 <BreadcrumbItem>
-                                    <BreadcrumbLink href="/" className="text-white text-lg hover:text-white hover:font-semibold hover:underline">{props.mainPage}</BreadcrumbLink>
+                                    <BreadcrumbLink href="/" className="text-green-400 text-lg hover:text-green-400 hover:font-semibold hover:underline">{props.mainPage}</BreadcrumbLink>
                                 </BreadcrumbItem>
                                 <BreadcrumbSeparator />
                                 <BreadcrumbItem>
-                                    <BreadcrumbPage className="text-white font-medium text-lg">{props.currentPage}</BreadcrumbPage>
+                                    <BreadcrumbPage className="text-green-400 font-medium text-lg">{props.currentPage}</BreadcrumbPage>
                                 </BreadcrumbItem>
                             </BreadcrumbList>
                         </Breadcrumb>
