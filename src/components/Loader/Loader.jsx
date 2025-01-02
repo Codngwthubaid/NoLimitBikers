102b46ade6ba2b3240d1b1d424a3d9256cfc67e5
Codngwthubaid <codngwthubaid@gmail.com>
2025-01-02 11:07:51 +0530

    Re-setup the src fodler

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import Loader from "../../assets/Loader.png"
+import MainLogo from "../../public/img/MainLogo.png"
 
 const loader = () => {
   
   return (
     <div className='bg-black overflow-hidden mx-auto z-10 flex justify-center items-center h-screen relative w-full'>
-        <Image className='transition-transform duration-1000 transform hover:scale-125' src={Loader} alt='Loading..'/>
+        <Image className='transition-transform duration-1000 transform hover:scale-125' src={MainLogo} alt='Loading..'/>
     </div>
   )
 }
