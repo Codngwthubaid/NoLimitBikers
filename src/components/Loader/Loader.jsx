9e88e109a65b954a60173eb1158e1487782d49ab
Codngwthubaid <codngwthubaid@gmail.com>
2025-01-03 23:41:51 +0530

    Loader Change

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import MainLogo from "../../public/img/MainLogo.png"
+import BikeLogo from "../../public/img/BikeLogo.png"
+
 
 const loader = () => {
   
   return (
     <div className='bg-black overflow-hidden mx-auto z-10 flex justify-center items-center h-screen relative w-full'>
-        <Image className='transition-transform duration-1000 transform hover:scale-125' src={MainLogo} alt='Loading..'/>
+        <Image className='transition-transform duration-1000 transform hover:scale-125' src={BikeLogo} alt='Loading..'/>
     </div>
   )
 }
