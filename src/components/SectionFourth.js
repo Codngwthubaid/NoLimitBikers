402c6abb60285544909bf1fb33c296d3a1ae912d
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-25 15:08:44 +0530

    Changes done

--- src/components/SectionFourth.js
+++ src/components/SectionFourth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import Image1 from '../public/Gallery/Image1.png'
-import Image2 from '../public/Gallery/Image2.png'
+import Image1 from '../public/Gallery/Image1.jpg'
+import Image2 from '../public/Gallery/Image2.jpg'
 
 
 const SectionFourth = () => {
