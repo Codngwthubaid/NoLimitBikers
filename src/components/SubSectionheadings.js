9791d94aabc439e336205f7f553fb37f3f04d283
Codngwthubaid <codngwthubaid@gmail.com>
2025-03-13 10:37:59 +0530

    re-add the image

--- src/components/SubSectionheadings.js
+++ src/components/SubSectionheadings.js
@@ -7,14 +7,14 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import HeroImage from "@/public/hero-section.png"
+import frontImage from "@/public/frontImage.jpg"
 
 const SubSectionheadings = (props) => {
     return (
         <>
             <main>
                 <div className='py-10 h-[50vh] gap-y-5 flex flex-col justify-center items-center bg-cover bg-center' style={{
-                    backgroundImage: `url(${HeroImage.src})`,
+                    backgroundImage: `url(${frontImage.src})`,
                     filter: 'grayscale(100%)',
                 }}>
                     <div className='text-5xl font-bold text-white'>{props.Head}</div>
