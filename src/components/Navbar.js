389b1aaffc6abe82c5c135521c5de5cc93924e16
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-02 07:14:19 +0530

    fixed bug of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,28 +9,28 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import Image from 'next/image';
-import MainLogo from "@/public/img/MainLogo.png"
+import MainLogo from "@/public/img/MainLogo.png";
 
+export default function Navbar() {
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
 
-const Navbar = () => {
-  const [mobileMenuActive, setMobileMenuActive] = useState(false);
-
-  const handleMobileMenu = () => {
-    setMobileMenuActive(!mobileMenuActive);
+  const handleLinkClick = () => {
+    setIsSheetOpen(false);
   };
 
   return (
-    <header className="bg-black fixed w-full z-20 ">
+    <header className="bg-black fixed w-full z-20">
       <nav className="container mx-auto p-4 flex justify-between items-center">
         <div className='flex justify-center items-center'>
           <Link href="/">
             <Image
               src={MainLogo}
               alt="Logo"
-              className='w-[300px] '
+              className='w-[300px]'
             />
           </Link>
         </div>
+
         <ul className="hidden lg:flex justify-between items-center">
           <li className="mr-8 p-1">
             <Link href="/" className="text-xl text-green-400 hover:font-semibold">Home</Link>
@@ -51,47 +51,48 @@ const Navbar = () => {
             <Link href="/pages/contact" className="text-xl text-green-400 hover:font-semibold">Contact</Link>
           </li>
         </ul>
+
         <div className='hidden lg:block'>
-          <button className='bg-green-500 text-black font-semibold text-lg p-3 rounded-md hover:text-white trnsition duration-300 ease-in-out'>
+          <button className='bg-green-500 text-black font-semibold text-lg p-3 rounded-md hover:text-white transition duration-300 ease-in-out'>
             <Link href="/pages/triallesson">DIRECT PROEFLES</Link>
           </button>
         </div>
-        <div className="lg:hidden flex justify-center" onClick={handleMobileMenu}>
-          <Sheet>
+
+        <div className="lg:hidden flex justify-center">
+          <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetTrigger>
-              <svg xmlns="http://www.w3.org/2000/svg" fill='white' x="0px" y="0px" width="40" height="40" viewBox="0 0 50 50">
-                <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" fill='white' width="40" height="40" viewBox="0 0 50 50">
+                <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z" />
               </svg>
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
                 <SheetDescription>
-                  <ul className='h-[100vh] flex flex-col justify-center items-center'>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
+                  <ul className='h-[100vh] flex flex-col justify-center items-center space-y-4'>
+                    <li onClick={handleLinkClick}>
                       <Link href="/" className="text-xl text-gray-700 hover:font-semibold">Home</Link>
                     </li>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
+                    <li onClick={handleLinkClick}>
                       <Link href="/pages/about" className="text-xl text-gray-700 hover:font-semibold">Over ons</Link>
                     </li>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
+                    <li onClick={handleLinkClick}>
                       <Link href="/pages/courses" className="text-xl text-gray-700 hover:font-semibold">Pakketten</Link>
                     </li>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
-                    <Link href="/pages/exams" className="text-xl text-gray-700 hover:font-semibold">Examens</Link>
+                    <li onClick={handleLinkClick}>
+                      <Link href="/pages/exams" className="text-xl text-gray-700 hover:font-semibold">Examens</Link>
                     </li>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
+                    <li onClick={handleLinkClick}>
                       <Link href="/pages/pricing" className="text-xl text-gray-700 hover:font-semibold">Tarieven</Link>
                     </li>
-                    <li onClick={handleMobileMenu} className="py-2 transition duration-300 ease-in-out">
+                    <li onClick={handleLinkClick}>
                       <Link href="/pages/contact" className="text-xl text-gray-700 hover:font-semibold">Contact</Link>
                     </li>
                     <li>
-                      <button className='bg-green-500 text-black font-semibold text-lg p-3 rounded-md'>
+                      <button onClick={handleLinkClick} className='bg-green-500 text-black font-semibold text-lg p-3 rounded-md'>
                         <Link href="/pages/triallesson">DIRECT PROEFLES</Link>
                       </button>
                     </li>
                   </ul>
-
                 </SheetDescription>
               </SheetHeader>
             </SheetContent>
@@ -99,8 +100,5 @@ const Navbar = () => {
         </div>
       </nav>
     </header>
-
   );
 };
-
-export default Navbar;
