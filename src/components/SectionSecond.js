bd530462e153754e7e447066cda7e875d2217214
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-10 07:16:17 +0530

    extra margin removes

--- src/components/SectionSecond.js
+++ src/components/SectionSecond.js
@@ -10,13 +10,13 @@ export default function SectionSecond() {
       </div>
       <div className="flex flex-col lg:flex-row gap-5 my-10">
         <div className="flex gap-x-5 justify-center items-center gap-y-5 flex-col mx-auto sm:flex-row">
-          <div className="flex justify-center items-center gap-5 flex-col sm:flex-row">
-            <div className='md: max-w-[45vw]'>
+          <div className="flex justify-center items-center gap-5 lg:mx-10 flex-col sm:flex-row">
+            <div className='mx-10 lg:mx-0'>
               <Card
                 data="🏍️ Motorrijlessen"
                 details="Bij NoLimitBikers bieden we motorrijlessen die aansluiten op jouw tempo en ervaring. Onze lessen zijn gericht op veiligheid, controle en plezier, zodat jij zonder zorgen je motorrijbewijs haalt." />
             </div>
-            <div className='md: max-w-[45vw]'>
+            <div className='mx-10 lg:mx-0'>
               <Card
                 data="🏁 AVB en AVD Dagcursussen"
                 details="Wil je snel en efficiënt je voertuigbeheersing (AVB) of verkeersdeelname (AVD) examen halen? Kies dan voor onze dagcursussen. Perfect voor iedereen die in één dag wil focussen op een specifiek onderdeel van het rijexamen." />
